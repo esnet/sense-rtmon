1e03c3257eb7390a35ef9d546acb9110f4dff8b5
PannuMuthu <30813477+PannuMuthu@users.noreply.github.com>
2021-12-21 17:22:30 -0800

    bug fixes

--- cleanEnv.py
+++ cleanEnv.py
@@ -5,12 +5,12 @@ import sys
 if len(sys.argv) <= 1:
     # Stop and remove all docker containers
     subprocess.run("sudo docker stop $(docker ps -aq)", shell=True)
-    subprocess.run("sudo docker rm $(docker ps -aq)", shell=True)
 # Flag to save Grafana container
 elif str(sys.argv[1]) == "--g" or str(sys.argv[1]) == "--G" or str(sys.argv[1]) == "--grafana":
     # Stop and remove all docker containers except for the Grafana container
-    subprocess.run("sudo docker stop $(docker ps -a -q | grep -v 'grafana')", shell=True)
-    subprocess.run("sudo docker rm $(docker ps -a -q | grep -v 'grafana')", shell=True)
+    subprocess.run("sudo docker stop $(docker ps -aq)", shell=True)
+    subprocess.run("sudo docker start grafana")
 # Remove SNMP Exporter generator and its dependencies
+subprocess.run("sudo docker rm $(docker ps -aq)", shell=True)
 subprocess.run("sudo yum -y remove gcc gcc-c++ make net-snmp net-snmp-utils net-snmp-libs net-snmp-devel", shell=True)
 subprocess.run("rm go1.13.linux-amd64.tar.gz.*", shell=True)

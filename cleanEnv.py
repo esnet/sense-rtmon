339c1d80d77a402a622d1dc3e604f6e63c5e86be
PannuMuthu <30813477+PannuMuthu@users.noreply.github.com>
2022-01-19 21:29:14 -0800

    now removes github installs

--- cleanEnv.py
+++ cleanEnv.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import subprocess
+import os
 import sys
 
 if len(sys.argv) <= 1:
@@ -13,4 +14,7 @@ elif str(sys.argv[1]) == "--g" or str(sys.argv[1]) == "--G" or str(sys.argv[1]) 
 # Remove SNMP Exporter generator and its dependencies
 subprocess.run("sudo docker rm $(docker ps -aq)", shell=True)
 subprocess.run("sudo yum -y remove gcc gcc-c++ make net-snmp net-snmp-utils net-snmp-libs net-snmp-devel", shell=True)
-subprocess.run("rm go1.13.linux-amd64.tar.gz.*", shell=True)
+dir = str(os.getcwd())
+goLoc = dir + "/SNMPExporter"
+subprocess.run("rm go1.13.linux-amd64.tar.gz", shell=True, cwd=goLoc)
+subprocess.run("rm go1.13.linux-amd64.tar.gz.*", shell=True, cwd=goLoc)

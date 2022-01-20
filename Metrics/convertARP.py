c01b517ee4d10b61e426824b294dd8a2d8952d87
PannuMuthu <30813477+PannuMuthu@users.noreply.github.com>
2022-01-20 11:31:07 -0800

    fixed regex

--- Metrics/convertARP.py
+++ Metrics/convertARP.py
@@ -10,11 +10,14 @@ with open(filename) as file:
 json_file = open('arp.json', 'w')
 json_file.write("{\n")
 for line in lines:
-    dump = {}
-    dump['hostname']  = re.findall(r'^(.*?)\s', line)[0]
-    #dump['mac'] = re.findall(r'([0-9a-f]{1,2}[\.:-]){5}([0-9a-f]{1,2})', line)[0]
-    dump['ip']  = re.findall(r'[0-9]+(?:\.[0-9]+){3}', line)[0]
-    json.dump(dump, json_file)
-    json_file.write(",\n")
+    try:
+        dump = {}
+        dump['hostname']  = re.findall(r'^(.*?)\s', line)[0]
+        dump['mac'] = re.findall(r'(?:[0-9a-fA-F]:?){12}', line)[0]
+        dump['ip']  = re.findall(r'[0-9]+(?:\.[0-9]+){3}', line)[0]
+        json.dump(dump, json_file)
+        json_file.write(",\n")
+    except IndexError:
+        continue
 json_file.write("}")
 json_file.close()

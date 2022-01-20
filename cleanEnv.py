4bf45b5f62c7f197dc16f8b2e1b3391e26e84279
PannuMuthu <30813477+PannuMuthu@users.noreply.github.com>
2022-01-20 12:47:10 -0800

    save grafana on default

--- cleanEnv.py
+++ cleanEnv.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python3
 import subprocess
-import os
 import sys
+import os
 
+# Defaults to save the running Grafana container if it exists
 if len(sys.argv) <= 1:
-    # Stop and remove all docker containers
-    subprocess.run("sudo docker stop $(docker ps -aq)", shell=True)
-    subprocess.run("sudo docker rm $(docker ps -aq)", shell=True)
-# Flag to save Grafana container
-elif str(sys.argv[1]) == "--g" or str(sys.argv[1]) == "--G" or str(sys.argv[1]) == "--grafana":
     # Stop and remove all docker containers except for the Grafana container
     subprocess.run("sudo docker stop $(docker ps -aq)", shell=True)
-    subprocess.run("sudo docker start grafana")
+    subprocess.run("sudo docker start grafana", shell=True)
+    subprocess.run("sudo docker rm $(docker ps -aq)", shell=True)
+# Flag to remove the running Grafana container
+elif str(sys.argv[1]) == "--a" or str(sys.argv[1]) == "--A" or str(sys.argv[1]) == "--all":
+    # Stop and remove all docker containers
+    subprocess.run("sudo docker stop $(docker ps -aq)", shell=True)
     subprocess.run("sudo docker rm $(docker ps -aq)", shell=True)
 # Remove SNMP Exporter generator and its dependencies
 subprocess.run("sudo docker rm $(docker ps -aq)", shell=True)
 subprocess.run("sudo yum -y remove gcc gcc-c++ make net-snmp net-snmp-utils net-snmp-libs net-snmp-devel", shell=True)
 dir = str(os.getcwd())
 goLoc = dir + "/SNMPExporter"
-subprocess.run("rm go1.13.linux-amd64.tar.gz", shell=True, cwd=goLoc)
-subprocess.run("rm go1.13.linux-amd64.tar.gz.*", shell=True, cwd=goLoc)
+subprocess.run("rm go1.13.linux-amd64.tar.gz", shell=True, cwd = goLoc)
+subprocess.run("rm go1.13.linux-amd64.tar.gz.*", shell=True, cwd = goLoc)

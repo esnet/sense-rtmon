a8bd9d7c6eb1fa4ae8e3aec121bd70ca9b4f6a5d
PannuMuthu <30813477+PannuMuthu@users.noreply.github.com>
2022-01-20 11:12:38 -0800

    arp -a

--- Metrics/hostMetrics.py
+++ Metrics/hostMetrics.py
@@ -14,8 +14,8 @@ with open(sys.argv[1], 'r') as stream:
         print("Config file load error!")
 
 tcpdumpCMDs = []
-tcpdumpCMDs.append("tcpdump -G 2 -W 1 -i" + data['hostA']['interfaceName'] + " -s 100 host " + data['hostA']['IP'] +" -nA -w tcpdumpA.pcap")
-tcpdumpCMDs.append("tcpdump -G 2 -W 1 -i" + data['hostB']['interfaceName'] + " -s 100 host " + data['hostB']['IP'] +" -nA -w tcpdumpB.pcap")
+tcpdumpCMDs.append("tcpdump -G " + str(data['scrapeDuration']) + " -W 1 -i" + data['hostA']['interfaceName'] + " -s 100 host " + data['hostA']['IP'] +" -nA -w tcpdumpA.pcap")
+tcpdumpCMDs.append("tcpdump -G " + str(data['scrapeDuration']) + " -W 1 -i" + data['hostB']['interfaceName'] + " -s 100 host " + data['hostB']['IP'] +" -nA -w tcpdumpB.pcap")
 
 for cmd in tcpdumpCMDs:
     subprocess.run(cmd, shell=True)
@@ -23,3 +23,6 @@ for cmd in tcpdumpCMDs:
 subprocess.run("tshark -r tcpdumpA.pcap -T json >tcpdumpA.json", shell=True)
 subprocess.run("tshark -r tcpdumpB.pcap -T json >tcpdumpB.json", shell=True)
 
+subprocess.run("arp -a > arpOut.txt", shell=True)
+
+subprocess.run("python3 convertARP.py arpOut.txt", shell=True)

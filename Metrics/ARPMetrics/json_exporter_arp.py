17ad9bbf6f72418536f9d3e0bf4d87f214aa87be
PannuMuthu <30813477+PannuMuthu@users.noreply.github.com>
2022-03-17 22:12:56 -0700

    integrated pushgateway

--- Metrics/ARPMetrics/json_exporter_arp.py
+++ Metrics/ARPMetrics/json_exporter_arp.py
@@ -2,6 +2,7 @@ from prometheus_client import start_http_server, Metric, REGISTRY
 import os
 import yaml
 import json
+import requests
 import sys
 import time
 from datetime import datetime
@@ -9,7 +10,6 @@ from subprocess import Popen, PIPE
 
 class JsonCollector(object):
   def collect(self):
-
     dir = str(os.getcwd())
     loc = dir + "/jsonFiles/"
     pastOut = ""
@@ -37,6 +37,9 @@ class JsonCollector(object):
             metric.add_sample(metricName, value=1, labels={'hostname': entry['hostname']})
             metric.add_sample(metricName, value=1, labels={'mac_address': entry['mac']})
             metric.add_sample(metricName, value=1, labels={'ip_address': entry['ip']})
+            payload = "Scrape_" + now.strftime("%Y_%m_%d_%H_%M_%S") + " 1\n"
+            url = "http://localhost:9091/metrics/job/arpMetrics/hostname/" + str(entry['hostname']) + "/mac_address/" + str(entry['mac']) + "/ip_address/" + str(entry['ip'])
+            push = requests.post(url, data=payload)
             count += 1
             yield metric
           except KeyError:
@@ -45,6 +48,9 @@ class JsonCollector(object):
         mName = "ARP_Entry_Count" + str(count) + "_Scrape_" + now.strftime("%Y_%m_%d_%H_%M_%S")
         metric = Metric("ARP_Entry_Count", "Number of ARP Entries", "summary")
         metric.add_sample("ARP_Entry_Count", value=(count-1), labels={})
+        url2 = "http://localhost:9091/metrics/job/arpMetrics/entryCount/" + str(count-1)
+        payload2 = "Scrape_" + now.strftime("%Y_%m_%d_%H_%M_%S") + " 1\n"
+        push2 = requests.post(url2, data=payload2)
         yield metric
 if __name__ == '__main__':
   # Usage: json_exporter.py port endpoint
@@ -56,6 +62,6 @@ if __name__ == '__main__':
       except yaml.YAMLError as exc:
           print("Config file load error!")
 
-  start_http_server(int(data['tcpPort']))
+  start_http_server(int(data['port']))
   REGISTRY.register(JsonCollector())
   while True: time.sleep(int(data['scrapeDuration']))

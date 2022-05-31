340bff5434c41c88723f58fcb4cc3160290904c3
PannuMuthu <30813477+PannuMuthu@users.noreply.github.com>
2022-05-31 12:02:41 -0700

    comment out container removal

--- cleanEnv.py
+++ cleanEnv.py
@@ -8,14 +8,14 @@ if len(sys.argv) <= 1:
     # Stop and remove all docker containers except for the Grafana container
     subprocess.run("sudo docker stop $(docker ps -aq)", shell=True)
     subprocess.run("sudo docker start grafana", shell=True)
-    subprocess.run("sudo docker rm $(docker ps -aq)", shell=True)
+    # subprocess.run("sudo docker rm $(docker ps -aq)", shell=True)
 # Flag to remove the running Grafana container
 elif str(sys.argv[1]) == "--a" or str(sys.argv[1]) == "--A" or str(sys.argv[1]) == "--all":
     # Stop and remove all docker containers
     subprocess.run("sudo docker stop $(docker ps -aq)", shell=True)
-    subprocess.run("sudo docker rm $(docker ps -aq)", shell=True)
+    # subprocess.run("sudo docker rm $(docker ps -aq)", shell=True)
 # Remove SNMP Exporter generator and its dependencies
-subprocess.run("sudo docker rm $(docker ps -aq)", shell=True)
+# subprocess.run("sudo docker rm $(docker ps -aq)", shell=True)
 subprocess.run("sudo yum -y remove gcc gcc-c++ make net-snmp net-snmp-utils net-snmp-libs net-snmp-devel", shell=True)
 subprocess.run("rm -R tcpFiles", shell=True)
 subprocess.run("rm -R jsonFiles", shell=True)

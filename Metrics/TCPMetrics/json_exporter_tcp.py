8243f89b4324ec9cea3a660e66f1f740f8167d27
Zhenbo <44856370+ZhenboYan@users.noreply.github.com>
2022-05-31 15:51:51 -0400

    Update json_exporter_tcp.py

--- Metrics/TCPMetrics/json_exporter_tcp.py
+++ Metrics/TCPMetrics/json_exporter_tcp.py
@@ -3,7 +3,9 @@ import yaml
 import json
 import sys
 import time
+import requests
 import os
+from datetime import datetime
 from subprocess import Popen, PIPE
 
 class JsonCollector(object):
@@ -23,11 +25,11 @@ class JsonCollector(object):
         time.sleep(2)
         with open(complete, 'r', encoding="utf-8") as f:
           response = json.load(f)
-
+        now  = datetime.now()
         count = 1
         for entry in response:
           try:
-            metricName = "Packet_" + str(count) + "_Scrape_" + str(output[6:-5]).replace("-","_")
+            metricName = "Packet_" + str(count) + "_Scrape_" + now.strftime("%Y_%m_%d_%H_%M_%S")
             metric = Metric(metricName, 'Packet Info Collected from TCPDUMP', 'summary')
             metric.add_sample(metricName, value=1, labels={'TCPSourcePort': entry['_source']['layers']['tcp']['tcp.srcport']})
             metric.add_sample(metricName, value=1, labels={'TCPDestinationPort': entry['_source']['layers']['tcp']['tcp.dstport']})
@@ -45,6 +47,9 @@ class JsonCollector(object):
             metric.add_sample(metricName, value=1, labels={'EthSource': entry['_source']['layers']['eth']['eth.src']})
             metric.add_sample(metricName, value=1, labels={'EthDestination': entry['_source']['layers']['eth']['eth.dst']})
             metric.add_sample(metricName, value=1, labels={'FrameProtocols': entry['_source']['layers']['frame']['frame.protocols']})
+            payload = "Scrape_" + now.strftime("%Y_%m_%d_%H_%M_%S") + " 1\n"
+            url = "http://localhost:9091/metrics/job/tcpMetrics/tcpSourcePort/" + str(entry['_source']['layers']['tcp']['tcp.srcport']) + "/tcpDestinationPort/" + str(entry['_source']['layers']['tcp']['tcp.dstport']) + "/TCPWindowSize/" + str(entry['_source']['layers']['tcp']['tcp.window_size_value']) + "/TCPFlags/" + str(entry['_source']['layers']['tcp']['tcp.flags']) +"/IPSource/" + str(entry['_source']['layers']['ip']['ip.src']) + "/IPDestination/" + str(entry['_source']['layers']['ip']['ip.dst'])
+            push = requests.post(url, data=payload)
             count += 1
             yield metric
           except KeyError:
@@ -53,6 +58,9 @@ class JsonCollector(object):
         mName = "TCP_Packet_Count"
         metric = Metric(mName, "Number of Packets Retrieved from TCPDUMP", "summary")
         metric.add_sample(mName, value=(count-1), labels={})
+        url2 = "http://localhost:9091/metrics/job/tcpMetrics/packetCount/" + str(count-1)
+        payload2 = "Scrape_" + now.strftime("%Y_%m_%d_%H_%M_%S") + " 1\n"
+        push2 = requests.post(url2, data=payload2)
         yield metric
 if __name__ == '__main__':
   # Usage: json_exporter.py port endpoint
@@ -63,6 +71,6 @@ if __name__ == '__main__':
       except yaml.YAMLError as exc:
           print("Config file load error!")
 
-  start_http_server(int(data['tcpPort']))
+  start_http_server(int(data['port']))
   REGISTRY.register(JsonCollector())
   while True: time.sleep(int(data['scrapeDuration']))

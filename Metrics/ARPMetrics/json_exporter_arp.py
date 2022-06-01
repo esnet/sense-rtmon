9df73da138a0f573624a5f72c55629d79351943d
Zhenbo <44856370+ZhenboYan@users.noreply.github.com>
2022-06-01 10:50:30 -0400

    dynamic URL
    URL is set according to the config file's receiver's (pushgateway) IP address.

--- Metrics/ARPMetrics/json_exporter_arp.py
+++ Metrics/ARPMetrics/json_exporter_arp.py
@@ -8,6 +8,17 @@ import time
 from datetime import datetime
 from subprocess import Popen, PIPE
 
+config_data ={}
+if __name__ == '__main__':
+  # Usage: json_exporter.py port endpoint
+  # Usage: json_exporter.py port endpoint
+  with open(sys.argv[1], 'r') as stream:
+      try:
+          config_data = yaml.safe_load(stream)
+      except yaml.YAMLError as exc:
+          print("Config file load error!")
+receiver_ip_address = "http://" + str(config_data['receiverIP'])
+
 class JsonCollector(object):
   def collect(self):
     dir = str(os.getcwd())
@@ -38,7 +49,7 @@ class JsonCollector(object):
             metric.add_sample(metricName, value=1, labels={'mac_address': entry['mac']})
             metric.add_sample(metricName, value=1, labels={'ip_address': entry['ip']})
             payload = "Scrape_" + now.strftime("%Y_%m_%d_%H_%M_%S") + " 1\n"
-            url = "http://localhost:9091/metrics/job/arpMetrics/hostname/" + str(entry['hostname']) + "/mac_address/" + str(entry['mac']) + "/ip_address/" + str(entry['ip'])
+            url = receiver_ip_address + ":9091/metrics/job/arpMetrics/hostname/" + str(entry['hostname']) + "/mac_address/" + str(entry['mac']) + "/ip_address/" + str(entry['ip'])
             push = requests.post(url, data=payload)
             count += 1
             yield metric
@@ -48,20 +59,13 @@ class JsonCollector(object):
         mName = "ARP_Entry_Count" + str(count) + "_Scrape_" + now.strftime("%Y_%m_%d_%H_%M_%S")
         metric = Metric("ARP_Entry_Count", "Number of ARP Entries", "summary")
         metric.add_sample("ARP_Entry_Count", value=(count-1), labels={})
-        url2 = "http://localhost:9091/metrics/job/arpMetrics/entryCount/" + str(count-1)
+        url2 = receiver_ip_address + ":9091/metrics/job/arpMetrics/entryCount/" + str(count-1)
         payload2 = "Scrape_" + now.strftime("%Y_%m_%d_%H_%M_%S") + " 1\n"
         push2 = requests.post(url2, data=payload2)
         yield metric
 if __name__ == '__main__':
   # Usage: json_exporter.py port endpoint
   # Usage: json_exporter.py port endpoint
-  data = {}
-  with open(sys.argv[1], 'r') as stream:
-      try:
-          data = yaml.safe_load(stream)
-      except yaml.YAMLError as exc:
-          print("Config file load error!")
-
   start_http_server(int(data['port']))
   REGISTRY.register(JsonCollector())
   while True: time.sleep(int(data['scrapeDuration']))

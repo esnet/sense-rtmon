29db3b9ea8fcb41fd2431a47f133ef3b7309311f
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-06-22 14:36:39 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ else
     touch /root/push_node_exporter_metrics.sh
     chmod +x /root/push_node_exporter_metrics.sh
     sudo tee /root/push_node_exporter_metrics.sh<<EOF
-    curl -s localhost:9100/metrics | curl --data-binary @- $pushgateway_server/metrics/job/node-exporter/instance/$MYIP
+curl -s localhost:9100/metrics | curl --data-binary @- $pushgateway_server/metrics/job/node-exporter/instance/$MYIP
 EOF
 fi
 
@@ -32,6 +32,33 @@ else
     touch /root/push_snmp_exporter_metrics.sh
     chmod +x /root/push_snmp_exporter_metrics.sh
     sudo tee /root/push_snmp_exporter_metrics.sh<<EOF
-    curl -s localhost:9116/metrics | curl --data-binary @- $pushgateway_server/metrics/job/snmp-exporter/instance/$MYIP
+curl -s localhost:9116/metrics | curl --data-binary @- $pushgateway_server/metrics/job/snmp-exporter/instance/$MYIP
 EOF
 fi
+
+if [ -f "/root/cron_autopush" ]; then
+    echo "cron_autopush already exits"
+else
+    touch /root/cron_autopush
+fi
+
+echo "!!    copy paste crontab to a temporary file"
+crontab -l > /root/cron_autopush
+
+if [ grep -Fxq "localhost:9116" /root/cron_autopush ]; then
+    echo "port 9116 in use in cron, type crontab -e to check"
+else
+    echo "Puppet Name: snmp exporter data to pushgateway every 15 seconds" >> /root/cron_autopush
+    echo "MAILTO=""" >> /root/cron_autopush
+    echo "* * * * * for i in 0 1 2; do /root/push_snmp_exporter_metrics.sh & sleep 15; done; /root/push_snmp_exporter_metrics.sh" >> /root/cron_autopush
+fi
+    if [ grep -Fxq "localhost:9100" /root/cron_autopush ]; then
+    echo "port 9100 in use in cron, type crontab -e to check"
+else
+    echo "Puppet Name: node exporter data to pushgateway every 15 seconds" >> /root/cron_autopush
+    echo "MAILTO=""" >> /root/cron_autopush
+    echo "* * * * * for i in 0 1 2; do /root/push_node_exporter_metrics.sh & sleep 15; done; /root/push_node_exporter_metrics.sh" >> /root/cron_autopush
+fi
+
+crontab /root/cron_autopush
+echo "!!    crontab set up complete"

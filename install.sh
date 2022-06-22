c4c044db45f7a7e09f4c2430e72103888cc2ce02
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-06-22 13:52:08 -0400

    install

--- install.sh
+++ install.sh
@@ -1,4 +1,37 @@
-echo "!!    Initialize Docker Swarm"
+#! /bin/bash
+# get correct IP address
 MYIP=$(hostname -I | head -n1 | awk '{print $1;}')
+read -r -p "Is ${MYIP} your IP address [y/N]: " correct_ip
+if [ "$correct_ip" == "N" ] || [ "$correct_ip" == "n" ]; then
+    read -r -p "Type in your ip address: " MYIP
+fi
+
+# get pushgateway server
+read -r -p "Enter Pushgateway server IP address (e.g. http://dev2.virnao.com:9091): " pushgateway_server
+
+echo "!!    Initialize Docker Swarm"
 echo "$MYIP is used for docker swarm advertise"
 docker swarm init --advertise-addr $MYIP
+
+echo "!!    Setting up Crontab to autopush Node and SNMP metrics to Pushgateway"
+if [ -f "/root/push_node_exporter_metrics.sh" ]; then
+    echo "push_node_exporter_metrics.sh already exits"
+    chmod +x /root/push_node_exporter_metrics.sh
+else
+    touch /root/push_node_exporter_metrics.sh
+    chmod +x /root/push_node_exporter_metrics.sh
+    sudo tee /root/push_node_exporter_metrics.sh<<EOF
+    curl -s localhost:9100/metrics | curl --data-binary @- $pushgateway_server/metrics/job/node-exporter/instance/$MYIP
+EOF
+fi
+
+if [ -f "/root/push_snmp_exporter_metrics.sh" ]; then
+    echo "push_snmp_exporter_metrics.sh already exits"
+    chmod +x /root/push_snmp_exporter_metrics.sh
+else
+    touch /root/push_snmp_exporter_metrics.sh
+    chmod +x /root/push_snmp_exporter_metrics.sh
+    sudo tee /root/push_snmp_exporter_metrics.sh<<EOF
+    curl -s localhost:9116/metrics | curl --data-binary @- $pushgateway_server/metrics/job/snmp-exporter/instance/$MYIP
+EOF
+fi

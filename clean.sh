baeec28bfd7a9c54819e6be79700f7cecc25bbe7
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-06-23 13:56:14 -0400

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
-
+echo "!!    Removing related Stack, Containers, Compose, and Images"
 docker stack rm site
-docker rm -f node-exporter snmp-exporter
+docker rm -f node-exporter snmp-exporter 
+# remove compose containers
+docker rm -f dynamicdashboard-snmp-exporter-1 dynamicdashboard-node-exporter-1
 docker image rm -f quay.io/prometheus/node-exporter prom/snmp-exporter
+echo "!!    Cleanning Complete"

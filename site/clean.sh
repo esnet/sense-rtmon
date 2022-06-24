05b901109b4a416357b939dba5aa511d6988cfda
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-06-24 16:06:29 -0400

    Update clean.sh

--- site/clean.sh
+++ site/clean.sh
@@ -5,4 +5,5 @@ docker rm -f node-exporter snmp-exporter arp-exporter tcp-exporter
 # remove compose containers
 docker rm -f site-node-exporter-1 site-snmp-exporter-1
 # docker image rm -f quay.io/prometheus/node-exporter prom/snmp-exporter
+docker image rm -f site_tcp-exporter site_arp-exporter arpexporter
 echo "!!    Cleanning Complete"

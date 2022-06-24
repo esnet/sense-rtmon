aef2bff194d3134429e0315813a45722f39b4462
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-06-24 15:59:40 -0400

    abs path

--- Metrics/ARPMetrics/arpMetrics.sh
+++ Metrics/ARPMetrics/arpMetrics.sh
@@ -2,5 +2,5 @@
 
 pip install pyyaml
 pip install prometheus_client
-python3 hostMetrics.py hostMetricConfig.yml & ( sleep 2 && python3 overwrite_json_exporter_arp.py hostMetricConfig.yml )
+python3 hostMetrics.py & ( sleep 2 && python3 overwrite_json_exporter_arp.py )
 trap "kill -- -$$" EXIT

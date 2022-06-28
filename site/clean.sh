a3b3294301ab9ed4bbe5c4533238b1c158dad91a
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-06-28 17:20:19 -0400

    script

--- site/clean.sh
+++ site/clean.sh
@@ -3,9 +3,9 @@ echo "!!    Removing related Stack, Containers, Compose"
 systemctl stop node_exporter
 docker stack rm site
 docker compose down -v
-docker rm -f node-exporter snmp-exporter arp-exporter tcp-exporter
+docker rm -f node-exporter snmp-exporter arp-exporter tcp-exporter arpexporter tcpexporter
 # remove compose containers
 docker rm -f site-node-exporter-1 site-snmp-exporter-1
 # docker image rm -f quay.io/prometheus/node-exporter prom/snmp-exporter
-docker image rm -f site_tcp-exporter site_arp-exporter arpexporter
+docker image rm -f site_tcp-exporter site_arp-exporter site_tcp-exporter
 echo "!!    Cleanning Complete"
--- site/start.sh
+++ site/start.sh
@@ -1,38 +1,58 @@
 #! /bin/bash
 
 echo "!!    Make sure Port 9100, 9116 are not in use"
-sleep 1
 echo "!!    sudo lsof -i -P -n | grep LISTEN"
 echo "!!    Check Port 9100 for node exporter"
 sudo lsof -i -P -n | grep 9100
-sleep 1
 echo "!!    Check Port 9116 for snmp exporter"
 sudo lsof -i -P -n | grep 9116
+
+read -r -p "Start Node Exporter? [y/N]: " start_node
+if [ "$start_node" == "y" ] || [ "$start_node" == "Y" ]; then
+    echo "Satring Node Exporter Service"
+    docker stack deploy -c node-exporter.yml site
+else 
+    echo "Skip Node Exporter"
+fi
+
+read -r -p "Start SNMP Exporter? [y/N]: " start_snmp
+if [ "$start_snmp" == "y" ] || [ "$start_snmp" == "Y" ]; then
+    echo "!!    Please configuring switch in snmpConfig.yml if needed"
+    echo "!!    DynamicDashboard/SNMPExporter/snmpConfig.yml"
+    read -p "Press enter to continue"
+    cd ../SNMPExporter
+    python3 dynamic.py snmpConfig.yml
+    cd ..
+    echo "Satring SNMP Exporter Service"
+    docker stack deploy -c snmp-exporter.yml site
+else 
+    echo "Skip SNMP Exporter"
+fi
+
+read -r -p "Start ARP Exporter? [y/N]: " start_arp
+if [ "$start_arp" == "y" ] || [ "$start_arp" == "Y" ]; then
+    echo "Satring ARP Exporter Service"
+    docker stack deploy -c arp-exporter.yml site
+else 
+    echo "Skip ARP Exporter"
+fi
+
+read -r -p "Start TCP Exporter? [y/N]: " start_tcp
+if [ "$start_tcp" == "y" ] || [ "$start_tcp" == "Y" ]; then
+    echo "Satring TCP Exporter Service"
+    docker stack deploy -c tcp-exporter.yml site
+else 
+    echo "Skip TCP Exporter"
+fi
+
+echo "!!    to start any exporter later, enter:"
+echo "docker stack deploy -c node-exporter.yml site"
+echo "docker stack deploy -c snmp-exporter.yml site"
+echo "!!    Please configuring switch in snmpConfig.yml if needed"
+echo "!!    DynamicDashboard/SNMPExporter/snmpConfig.yml"
+echo "docker stack deploy -c arp-exporter.yml site"
+echo "docker stack deploy -c tcp-exporter.yml site"
+
 sleep 1
 
-# all exporter started
-echo "!!    Config SNMP (can be shut down later)"
-cd ..
-cd SNMPExporter
-python3 dynamic.py snmpConfig.yml
-cd ..
-docker stack deploy -c docker-stack.yml site
-
-echo "!!    NODE, SNMP, ARP Exporters (technically everthing already started, shut down one at the time)"
-
-read -r -p "Start Node Exporter [y/N]: " start_node
-# shut down node exporter
-if [ "$start_node" == "n" ] || [ "$start_node" == "N" ]; then
-    echo "Node Exporter removed from the service"
-    docker service rm site_node_exporter
-else 
-    echo "Node Exporter started"
-fi
-
-read -r -p "Start SNMP Exporter [y/N]: " start_snmp
-if [ "$start_snmp" == "n" ] || [ "$start_snmp" == "N" ]; then
-    echo "SNMP Exporter removed from the service"
-    docker service rm site_node_exporter
-else 
-    echo "SNMP Exporter started"
-fi
+echo "!!    to remove site stack run ./clean.sh"

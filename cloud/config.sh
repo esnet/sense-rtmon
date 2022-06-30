e65b98a5ca7d27106c4a5d82cd936a5977b04ba0
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-06-30 15:04:24 -0400

    configuration set up

--- cloud/config.sh
+++ cloud/config.sh
@@ -6,26 +6,33 @@ if [ "$correct_ip" == "N" ] || [ "$correct_ip" == "n" ]; then
     read -r -p "Type in your ip address: " MYIP
 fi
 
-read -r -p "Enter number of host: " num_switch 
-if [ "$num_switch" == "1" ] ; then
-    read -r -p "Type in your switch ip address: " siwtchIP1
-else 
-    if [ "$num_switch" == "2" ] ; then
-        read -r -p "Type in your switch 1 ip address: " siwtchIP1
-        read -r -p "Type in your switch 2 ip address: " siwtchIP2
-    fi
-fi 
-
+# change args and config files
 read -r -p "Enter host 1 ip address: " host1 
 read -r -p "Enter host 2 ip address: " host2 
+read -r -p "Enter number of host: " num_switch 
+read -r -p "Enter DNS server name for https, enter ip address for http: " server_ip
 
-sed -i -e "s@198.32.43.16@${host1}@" .se_config/argsDef.sh
-sed -i -e "s@198.32.43.16@${host1}@" .se_config/args.sh
-sed -i -e "s@198.32.43.16@${host1}@" .se_config/multiDef.sh
+sed -i -e "s@.*grafanaHostIP: .*@grafanaHostIP: =${server_ip}@" ../config.yml
 
-# config the input host ip in argsDef 
+sed -i -e "s@.*pushgateway=1.*@pushgateway=${MYIP}@" .se_config/argsDef.sh
+sed -i -e "s@.*instance=1.*@instance=${host1}@" .se_config/argsDef.sh
+sed -i -e "s@.*ip_address=1.*@ip_address=${host2}@" .se_config/argsDef.sh
+sed -i -e "s@.*netElNum=.*@netElNum=${num_switch}@" .se_config/argsDef.sh 
 
-# change localhost to ip address of the host
+# echo "optional "
+if [ "$num_switch" == "1" ] ; then
+    read -r -p "Type in your switch ip address: " switchIP1
+else 
+    if [ "$num_switch" == "2" ] ; then
+        read -r -p "Type in your switch 1 ip address: " switchIP1
+        read -r -p "Type in your switch 2 ip address: " switchIP2
+        sed -i -e "s@.*netElIP2=1.*@netElIP2=${switchIP2}@" .se_config/argsDef.sh 
+    fi
+fi
+
+if [ "$num_switch" != "0" ] ; then
+    sed -i -e "s@.*netElIP=1.*@netElIP=${switchIP1}@" .se_config/argsDef.sh 
+fi 
 
 echo "||    Inserting ${MYIP} to prometheus.yml file"
 echo "||    If the IP address is incorrect please update manually"
---
+++ site/config.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+MYIP=$(hostname -I | head -n1 | awk '{print $1;}')
+
+read -r -p "Is ${MYIP} your IP address [y/N]: " correct_ip
+if [ "$correct_ip" == "N" ] || [ "$correct_ip" == "n" ]; then
+    read -r -p "Type in your ip address: " MYIP
+fi
+
+# change args and config files
+read -r -p "Enter host 1 ip address: " host1 
+read -r -p "Enter host 2 ip address: " host2 
+read -r -p "Enter number of host: " num_switch 
+read -r -p "Enter DNS server name for https, enter ip address for http: " server_ip
+
+sed -i -e "s@.*grafanaHostIP: .*@grafanaHostIP: '${server_ip}'@" ../config.yml
+
+sed -i -e "s@.*hostIP: .*@hostIP: ${MYIP}@" ../config.yml

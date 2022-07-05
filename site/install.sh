f3180ca30a2787d713119551bfee7caabf6cd21a
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-07-05 16:27:06 -0400

    Update install.sh

--- site/install.sh
+++ site/install.sh
@@ -114,16 +114,16 @@ fi
 #     echo "* * * * * for i in 0 1 2; do /root/push_node_exporter_metrics.sh & sleep 15; done; /root/push_node_exporter_metrics.sh" >> /root/cron_autopush
 # fi
 
-if grep -F "/root/update_arp_exporter.sh" /root/cron_autopush
-then    
-    echo "task is already in cron, type crontab -e to check"
-else
-    echo "#Puppet Name: check update on arp table every 15 seconds" >> /root/cron_autopush
-    echo "MAILTO=""" >> /root/cron_autopush
-    echo "* * * * * for i in 0 1 2; do /root/update_arp_exporter.sh & sleep 15; done; /root/update_arp_exporter.sh" >> /root/cron_autopush
-fi
+# if grep -F "/root/update_arp_exporter.sh" /root/cron_autopush
+# then    
+#     echo "task is already in cron, type crontab -e to check"
+# else
+#     echo "#Puppet Name: check update on arp table every 15 seconds" >> /root/cron_autopush
+#     echo "MAILTO=""" >> /root/cron_autopush
+#     echo "* * * * * for i in 0 1 2; do /root/update_arp_exporter.sh & sleep 15; done; /root/update_arp_exporter.sh" >> /root/cron_autopush
+# fi
 
-echo ""
-crontab /root/cron_autopush
-rm -f /root/cron_autopush
-echo "!!    crontab set up successfuly"
+# echo ""
+# crontab /root/cron_autopush
+# rm -f /root/cron_autopush
+# echo "!!    crontab set up successfuly"

991d9e347eba6f427acf811833db4b1f0a18e3de
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-07-05 16:11:18 -0400

    Update install.sh

--- site/install.sh
+++ site/install.sh
@@ -1,66 +1,66 @@
-#! /bin/bash
+# #! /bin/bash
 
-# check docker 
-if [ -x "$(command -v docker)" ]; then
-    echo "||        Found docker..."
-    echo "||        Running docker login..."
-    docker login
-else
-    echo "!!    Docker command not found."
-    echo "!!    Please visit https://docs.docker.com/install/ for installation instructions."
-    exit 1
-fi
+# # check docker 
+# if [ -x "$(command -v docker)" ]; then
+#     echo "||        Found docker..."
+#     echo "||        Running docker login..."
+#     docker login
+# else
+#     echo "!!    Docker command not found."
+#     echo "!!    Please visit https://docs.docker.com/install/ for installation instructions."
+#     exit 1
+# fi
 
-# check docker compose
-if [ -x "$(command -v docker compose)" ]; then
-    echo "||        Found docker compose..."
-    echo "||        Running docker login..."
-    docker login
-else
-    echo "!!    Docker compose command not found."
-    echo "!!    Installing docker compose"
-    suod yum install -y docker-compose-plugin
-    docker login
-    # exit 1
-fi
+# # check docker compose
+# if [ -x "$(command -v docker compose)" ]; then
+#     echo "||        Found docker compose..."
+#     echo "||        Running docker login..."
+#     docker login
+# else
+#     echo "!!    Docker compose command not found."
+#     echo "!!    Installing docker compose"
+#     suod yum install -y docker-compose-plugin
+#     docker login
+#     # exit 1
+# fi
 
-# get correct IP address
-MYIP=$(hostname -I | head -n1 | awk '{print $1;}')
-read -r -p "Is ${MYIP} your IP address [y/N]: " correct_ip
-if [ "$correct_ip" == "N" ] || [ "$correct_ip" == "n" ]; then
-    read -r -p "Type in your ip address: " MYIP
-fi
+# # get correct IP address
+# MYIP=$(hostname -I | head -n1 | awk '{print $1;}')
+# read -r -p "Is ${MYIP} your IP address [y/N]: " correct_ip
+# if [ "$correct_ip" == "N" ] || [ "$correct_ip" == "n" ]; then
+#     read -r -p "Type in your ip address: " MYIP
+# fi
 
-# get pushgateway server
-read -r -p "Enter Pushgateway server IP address (e.g. http://dev2.virnao.com:9091): " pushgateway_server
+# # get pushgateway server
+# read -r -p "Enter Pushgateway server IP address (e.g. http://dev2.virnao.com:9091): " pushgateway_server
 
-echo "!!    Initialize Docker Swarm"
-echo "$MYIP is used for docker swarm advertise"
-docker swarm init --advertise-addr $MYIP
+# echo "!!    Initialize Docker Swarm"
+# echo "$MYIP is used for docker swarm advertise"
+# docker swarm init --advertise-addr $MYIP
 
-echo "!!    Setting up Crontab to autopush Node and SNMP metrics to Pushgateway"
-if [ -f "/root/push_node_exporter_metrics.sh" ]; then
-    echo "push_node_exporter_metrics.sh already exits"
-    chmod +x /root/push_node_exporter_metrics.sh
-else
-    touch /root/push_node_exporter_metrics.sh
-    chmod +x /root/push_node_exporter_metrics.sh
-    sudo tee /root/push_node_exporter_metrics.sh<<EOF
-curl -s ${MYIP}:9100/metrics | curl --data-binary @- $pushgateway_server/metrics/job/node-exporter/instance/$MYIP
-EOF
-fi
+# echo "!!    Setting up Crontab to autopush Node and SNMP metrics to Pushgateway"
+# if [ -f "/root/push_node_exporter_metrics.sh" ]; then
+#     echo "push_node_exporter_metrics.sh already exits"
+#     chmod +x /root/push_node_exporter_metrics.sh
+# else
+#     touch /root/push_node_exporter_metrics.sh
+#     chmod +x /root/push_node_exporter_metrics.sh
+#     sudo tee /root/push_node_exporter_metrics.sh<<EOF
+# curl -s ${MYIP}:9100/metrics | curl --data-binary @- $pushgateway_server/metrics/job/node-exporter/instance/$MYIP
+# EOF
+# fi
 
-echo ""
-if [ -f "/root/push_snmp_exporter_metrics.sh" ]; then
-    echo "push_snmp_exporter_metrics.sh already exits"
-    chmod +x /root/push_snmp_exporter_metrics.sh
-else
-    touch /root/push_snmp_exporter_metrics.sh
-    chmod +x /root/push_snmp_exporter_metrics.sh
-    sudo tee /root/push_snmp_exporter_metrics.sh<<EOF
-curl -s ${MYIP}:9116/metrics | curl --data-binary @- $pushgateway_server/metrics/job/snmp-exporter/instance/$MYIP
-EOF
-fi
+# echo ""
+# if [ -f "/root/push_snmp_exporter_metrics.sh" ]; then
+#     echo "push_snmp_exporter_metrics.sh already exits"
+#     chmod +x /root/push_snmp_exporter_metrics.sh
+# else
+#     touch /root/push_snmp_exporter_metrics.sh
+#     chmod +x /root/push_snmp_exporter_metrics.sh
+#     sudo tee /root/push_snmp_exporter_metrics.sh<<EOF
+# curl -s ${MYIP}:9116/metrics | curl --data-binary @- $pushgateway_server/metrics/job/snmp-exporter/instance/$MYIP
+# EOF
+# fi
 
 
 echo ""
@@ -80,8 +80,8 @@ python3 convertARP.py ./arpFiles/arpOut-.txt ./jsonFiles/arpOut-.json
 EOF
 fi
 
-# install dependencies
-yum install -y p7zip p7zip-plugins make
+# # install dependencies
+# yum install -y p7zip p7zip-plugins make
 
 
 # create a temporary copy paste file
@@ -92,27 +92,27 @@ else
     touch /root/cron_autopush
 fi
 
-echo "!!    copy paste crontab to a temporary file"
-crontab -l > /root/cron_autopush
+# echo "!!    copy paste crontab to a temporary file"
+# crontab -l > /root/cron_autopush
 
-# check if job is alread in
-if grep -F "/root/push_snmp_exporter_metrics.sh" /root/cron_autopush 
-then
-    echo "task is already in cron, type crontab -e to check"
-else
-    echo "#Puppet Name: snmp exporter data to pushgateway every 15 seconds" >> /root/cron_autopush
-    echo "MAILTO=""" >> /root/cron_autopush
-    echo "* * * * * for i in 0 1 2; do /root/push_snmp_exporter_metrics.sh & sleep 15; done; /root/push_snmp_exporter_metrics.sh" >> /root/cron_autopush
-fi
+# # check if job is alread in
+# if grep -F "/root/push_snmp_exporter_metrics.sh" /root/cron_autopush 
+# then
+#     echo "task is already in cron, type crontab -e to check"
+# else
+#     echo "#Puppet Name: snmp exporter data to pushgateway every 15 seconds" >> /root/cron_autopush
+#     echo "MAILTO=""" >> /root/cron_autopush
+#     echo "* * * * * for i in 0 1 2; do /root/push_snmp_exporter_metrics.sh & sleep 15; done; /root/push_snmp_exporter_metrics.sh" >> /root/cron_autopush
+# fi
 
-if grep -F "/root/push_node_exporter_metrics.sh" /root/cron_autopush
-then    
-    echo "task is already in cron, type crontab -e to check"
-else
-    echo "#Puppet Name: node exporter data to pushgateway every 15 seconds" >> /root/cron_autopush
-    echo "MAILTO=""" >> /root/cron_autopush
-    echo "* * * * * for i in 0 1 2; do /root/push_node_exporter_metrics.sh & sleep 15; done; /root/push_node_exporter_metrics.sh" >> /root/cron_autopush
-fi
+# if grep -F "/root/push_node_exporter_metrics.sh" /root/cron_autopush
+# then    
+#     echo "task is already in cron, type crontab -e to check"
+# else
+#     echo "#Puppet Name: node exporter data to pushgateway every 15 seconds" >> /root/cron_autopush
+#     echo "MAILTO=""" >> /root/cron_autopush
+#     echo "* * * * * for i in 0 1 2; do /root/push_node_exporter_metrics.sh & sleep 15; done; /root/push_node_exporter_metrics.sh" >> /root/cron_autopush
+# fi
 
 if grep -F "/root/update_arp_exporter.sh" /root/cron_autopush
 then    

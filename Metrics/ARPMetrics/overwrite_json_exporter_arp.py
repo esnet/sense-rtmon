3b7d50153fc3e94f6986b409d02c81b892a89e70
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-07-07 15:12:54 -0400

    Update overwrite_json_exporter_arp.py

--- Metrics/ARPMetrics/overwrite_json_exporter_arp.py
+++ Metrics/ARPMetrics/overwrite_json_exporter_arp.py
@@ -27,56 +27,65 @@ class JsonCollector(object):
   def collect(self):
     # Fetch the JSON
     dir = str(os.getcwd()) + "/jsonFiles/"
-    arpout_json = dir + "arpOut.json"
-    f = open(arpout_json)
-    lines = f.readlines()
+    output_file =  dir + "arpOut.json"
+    previous_file = dir + "prev.json"
+    cur_file = open(output_file)
+    cur_lines = cur_file.readlines()
+    pre_file = open(previous_file)
+    pre_lines = pre_file.readlines()
+    time.sleep(1)
+    if pre_lines != cur_lines:
 
-    # loads the json format in
-    response = []
-    for line in lines[1:-1]:
-      response.append(json.loads(line[:-2]))
-    count = 1
-    
-    # delete previous urls
-    delete_file_path = dir + "delete.json"
-    with open(delete_file_path,"rt") as fp:
-      load_delete = json.load(fp)
-      for each_url in load_delete:
-        delete = requests.delete(each_url)
-    delete_list = []
-    
-    # post to pushgateway website
-    for entry in response:
-      metricName = "ARP_Entry_" + str(count) + "_Scrape"
-      metric = Metric(metricName, 'ARP Entry', 'summary')
-      hostname = entry['hostname']
-      if hostname == "?":
-        hostname = "no_name"
-      else:
+      arpout_json = dir + "arpOut.json"
+      f = open(arpout_json)
+      lines = f.readlines()
+
+      # loads the json format in
+      response = []
+      for line in lines[1:-1]:
+        response.append(json.loads(line[:-2]))
+      count = 1
+      
+      # delete previous urls
+      delete_file_path = dir + "delete.json"
+      with open(delete_file_path,"rt") as fp:
+        load_delete = json.load(fp)
+        for each_url in load_delete:
+          delete = requests.delete(each_url)
+      delete_list = []
+      
+      # post to pushgateway website
+      for entry in response:
+        metricName = "ARP_Entry_" + str(count) + "_Scrape"
+        metric = Metric(metricName, 'ARP Entry', 'summary')
         hostname = entry['hostname']
-      metric.add_sample(metricName, value=1, labels={'hostname': hostname})
-      metric.add_sample(metricName, value=1, labels={'mac_address': entry['mac']})
-      metric.add_sample(metricName, value=1, labels={'ip_address': entry['ip']})
-      # arbitrary pay load data is stored inside url
-      payload = "ARP_Table " + str(count) + "\n"
-      url = f"{receiver_ip_address}:9091/metrics/job/arpMetrics/instance/{instance_ip}/hostname/{str(hostname)}/mac_address/{str(entry['mac'])}/ip_address/{str(entry['ip'])}"
-      push = requests.post(url, data=payload)
-      delete_list.append(url)
-      count += 1
+        if hostname == "?":
+          hostname = "no_name"
+        else:
+          hostname = entry['hostname']
+        metric.add_sample(metricName, value=1, labels={'hostname': hostname})
+        metric.add_sample(metricName, value=1, labels={'mac_address': entry['mac']})
+        metric.add_sample(metricName, value=1, labels={'ip_address': entry['ip']})
+        # arbitrary pay load data is stored inside url
+        payload = "ARP_Table " + str(count) + "\n"
+        url = f"{receiver_ip_address}:9091/metrics/job/arpMetrics/instance/{instance_ip}/hostname/{str(hostname)}/mac_address/{str(entry['mac'])}/ip_address/{str(entry['ip'])}"
+        push = requests.post(url, data=payload)
+        delete_list.append(url)
+        count += 1
+        yield metric
+
+      # store delete list
+      with open(delete_file_path,"wt") as fp:
+        json.dump(delete_list,fp)
+
+      # mName = "ARP_Entry_Count" + str(count) + "_Scrape"
+      mName = "ARP_Entry_Count"
+      metric = Metric(mName, "Number of ARP Entries", "summary")
+      metric.add_sample(mName, value=(count-1), labels={})
+      url2 = f"{receiver_ip_address}:9091/metrics/job/arpMetrics/instance/{instance_ip}/entryCount/value"
+      payload2 = f"ARP_Entry_Count {str(count-1)}\n"
+      push2 = requests.post(url2, data=payload2)
       yield metric
-
-    # store delete list
-    with open(delete_file_path,"wt") as fp:
-      json.dump(delete_list,fp)
-
-    # mName = "ARP_Entry_Count" + str(count) + "_Scrape"
-    mName = "ARP_Entry_Count"
-    metric = Metric(mName, "Number of ARP Entries", "summary")
-    metric.add_sample(mName, value=(count-1), labels={})
-    url2 = f"{receiver_ip_address}:9091/metrics/job/arpMetrics/instance/{instance_ip}/entryCount/value"
-    payload2 = f"ARP_Entry_Count {str(count-1)}\n"
-    push2 = requests.post(url2, data=payload2)
-    yield metric
 
         
 if __name__ == '__main__':
@@ -84,19 +93,13 @@ if __name__ == '__main__':
   start_http_server(int(config_data['arpMetrics']['port']))
   # REGISTRY.register(JsonCollector())
   dir = str(os.getcwd()) + "/jsonFiles/"
-  output_file =  dir + "arpOut.json"
-  previous_file = dir + "prev.json"
 
   while True:
     # time.sleep(1)
-    cur_file = open(output_file)
-    cur_lines = cur_file.readlines()
-    pre_file = open(previous_file)
-    pre_lines = pre_file.readlines()
-    if pre_lines != cur_lines:
-      REGISTRY.register(JsonCollector())
-      time.sleep(1)
-      REGISTRY = CollectorRegistry(auto_describe=True) # solves duplicate entry problem
+
+    REGISTRY.register(JsonCollector())
+    time.sleep(1)
+    REGISTRY = CollectorRegistry(auto_describe=True) # solves duplicate entry problem
     # CollectorRegistry.clear()
 
   # time.sleep(int(config_data['arpMetrics']['scrapeDuration']))

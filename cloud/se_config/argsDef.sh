f8950558fde4e56ed1f6e1b5b9e6777dc799c9ba
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-07-08 14:59:27 -0400

    Update argsDef.sh

--- cloud/se_config/argsDef.sh
+++ cloud/se_config/argsDef.sh
@@ -23,7 +23,8 @@ netElNum=1
 netElIP=198.32.43.1
 # network element 2 ip address
 netElIP2=172.16.1.14
-
+host1=$instance
+host2=$ip_address
 output=$(ping -c 1 "${ip_address}" 2>/dev/null)
 
 echo "# HELP IP address of target remote host"
@@ -40,16 +41,40 @@ if [ $? -eq 0 ]; then
     # If success, double check that the MAC address for the other host is in the ARP table
     echo "# HELP Metric checks whether remote host exists in ARP table of current host";
 
+    # ARP check
     if curl ${pushgateway}:9091/metrics | grep "instance=\"${instance}\",ip_address=\"${ip_address}\""; then
         echo "host1_has_host2_arp{host=\"${instance}\"} 1";
     else 
         echo "host1_has_host2_arp{host=\"${instance}\"} 0";
     fi
-
     if curl ${pushgateway}:9091/metrics | grep "instance=\"${ip_address}\",ip_address=\"${instance}\""; then
         echo "host2_has_host1_arp{host=\"${ip_address}\"} 1";
     else 
-        echo "host2_has_host1_arp{host=\"${instance}\"} 0";
+        echo "host2_has_host1_arp{host=\"${ip_address}\"} 0";
+    fi
+
+    # ping check
+    if curl ${pushgateway}:9091/metrics | grep ".*instance=\"${host1}\".*ping_status=\"1\".*ping_this_ip=\"${host2}\".*"; then 
+        echo "host1_ping_status{host=\"${host1}\"} 1"
+    else 
+        echo "host1_ping_status{host=\"${host1}\"} 0"
+    fi
+    if curl ${pushgateway}:9091/metrics | grep ".*instance=\"${host2}\".*ping_status=\"1\".*ping_this_ip=\"${host1}\".*"; then 
+        echo "host2_ping_status{host=\"${host2}\"} 1"
+    else 
+        echo "host2_ping_status{host=\"${host2}\"} 0"
+    fi
+    
+    # SNMP mac address check
+    if curl ${pushgateway}:9091/metrics | grep ".*instance=\"${host1}\".*ip_address=\"${netElIP}\".*mac_address.*"
+        echo "host1_snmp_mac_status{host=\"${host1}\"} 1"
+    else 
+        echo "host1_snmp_mac_status{host=\"${host1}\"} 0"
+    fi
+    if curl ${pushgateway}:9091/metrics | grep ".*instance=\"${host2}\".*ip_address=\"${netElIP}\".*mac_address.*"
+        echo "host2_snmp_mac_status{host=\"${host2}\"} 1"
+    else 
+        echo "host2_snmp_mac_status{host=\"${host2}\"} 0"
     fi
 
     if curl ${pushgateway}:9091/metrics | grep "instance=\"${instance}\",ip_address=\"${ip_address}\""; then

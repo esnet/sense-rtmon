9d0067d9e94f14c141cacc78051353efe178085d
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-07-12 16:46:59 -0400

    deletion

--- site/clean.sh
+++ site/clean.sh
@@ -8,6 +8,12 @@ docker rm -f node-exporter snmp-exporter arp-exporter tcp-exporter arpexporter t
 docker rm -f site-node-exporter-1 site-snmp-exporter-1
 # docker image rm -f quay.io/prometheus/node-exporter prom/snmp-exporter
 docker image rm -f site_tcp-exporter site_arp-exporter site_tcp-exporter arp_exporter:latest tcp_exporter:latest
-echo "!!    Erase pushgateway urls sent from this host"
-python3 erase_pushgateway.py
-echo "!!    Cleanning Complete"
+
+read -r -p "Erase Metrics [y/N]: " erase
+if [ "$erase" == "y" ] || [ "$erase" == "Y" ]; then
+    echo "!!    Erase pushgateway urls sent from this host"
+    python3 erase_pushgateway.py
+    echo "!!    Cleanning Complete"
+else 
+    echo "Nothing Erased"
+fi
--- site/erase_pushgateway.py
+++ site/erase_pushgateway.py
@@ -2,6 +2,18 @@
 import json
 import os
 import requests
+import yaml
+
+# get this host's IP address
+owd = os.getcwd()
+os.chdir("..")
+infpth = str(os.path.abspath(os.curdir)) + "/config.yml"
+os.chdir(owd)
+data = {}
+with open(infpth, 'r') as stream:
+    data = yaml.safe_load(stream)
+
+hostip = data['hostIP']
 
 dir = str(os.getcwd())    
 # delete previous urls
@@ -12,5 +24,8 @@ with open(delete_file_path,"rt") as fp:
         load_delete = json.load(fp)
         for each_url in load_delete:
             requests.delete(each_url)
-    delete_list = []
-    
+
+node_url = f"http://dev2.virnao.com:9091/metrics/job/node-exporter/instance/{hostip}"
+snmp_url = f"http://dev2.virnao.com:9091/metrics/job/snmp-exporter/instance/{hostip}"
+requests.delete(node_url)
+requests.delete(snmp_url)

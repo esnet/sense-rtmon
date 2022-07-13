e3872cd159e4f4e8e8821fdb2cc35daf9fb8b38b
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-07-13 14:51:55 -0400

    generator update

--- SNMPExporter/dynamic_config.py
+++ SNMPExporter/dynamic_config.py
@@ -48,6 +48,7 @@ print("Configuring SNMP Exporter Generator...")
 dir = str(os.getcwd())
 genLoc = dir + "/src/github.com/prometheus/snmp_exporter/generator"
 genCmd = "yes | cp -rfa generator.yml " + genLoc
+subprocess.run(genCmd, shell=True)
 
 print("Generating dynamic SNMP config file...")
 subprocess.run("./generator generate", shell=True, cwd=genLoc)

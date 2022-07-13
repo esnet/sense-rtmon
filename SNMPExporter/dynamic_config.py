17b1bc835402994fa549cafe70362ac03195074f
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-07-13 12:05:01 -0400

    Update dynamic_config.py

--- SNMPExporter/dynamic_config.py
+++ SNMPExporter/dynamic_config.py
@@ -43,19 +43,13 @@ for k,v in replacements.items():
 print("Writing SNMP Exporter generator config file...")
 with open('generator.yml', 'w') as file:
     file.write(filedata)
+    
 print("Configuring SNMP Exporter Generator...")
-
-subprocess.run("sudo yum -y install p7zip p7zip-plugins gcc gcc-c++ make net-snmp net-snmp-utils net-snmp-libs net-snmp-devel", shell=True)
-os.environ["PATH"] += os.pathsep + os.pathsep.join(["/usr/local/go/bin"])
 dir = str(os.getcwd())
-os.putenv("GOPATH", dir)
-subprocess.run("go get github.com/prometheus/snmp_exporter/generator", shell=True)
 genLoc = dir + "/src/github.com/prometheus/snmp_exporter/generator"
 genCmd = "yes | cp -rfa generator.yml " + genLoc
-subprocess.run(genCmd, shell=True)
-subprocess.run("go build", shell=True, cwd=genLoc)
-subprocess.run("make mibs", shell=True, cwd=genLoc)
+
 print("Generating dynamic SNMP config file...")
 subprocess.run("./generator generate", shell=True, cwd=genLoc)
-
+subprocess.run("yes | cp -rfa snmp.yml ../../../../../", shell=True, cwd=genLoc)
 print("Success! Configured custom SNMP Exporter container")

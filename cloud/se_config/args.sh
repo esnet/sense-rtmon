78f5f700b6b9739212c0b91805055cf5e1df796c
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-07-15 15:13:28 -0400

    Update args.sh

--- cloud/se_config/args.sh
+++ cloud/se_config/args.sh
@@ -79,13 +79,13 @@ fi
 # SNMP mac address check switch 1
 if curl ${pushgateway}:9091/metrics | grep ".*instance=\"${host1}\".*ip_address=\"${switch_ip1}\".*mac_address.*"; then
     echo "host1_snmp_mac_status{host=\"${host1}\"} 1"
-    echo "switch1_mac{mac=\"${switch1_mac_no_quote}\"} switch1_mac_no_quote"
+    echo "switch1_mac{switch1_mac_no_quote=\"${switch1_mac_no_quote}\"} 1"
 else 
     echo "host1_snmp_mac_status{host=\"${host1}\"} 0"
 fi
 if curl ${pushgateway}:9091/metrics | grep ".*instance=\"${host2}\".*ip_address=\"${switch_ip1}\".*mac_address.*"; then
     echo "host2_snmp_mac_status{host=\"${host2}\"} 1"
-    echo "switch1_mac{mac=\"${switch1_mac_no_quote}\"} switch1_mac_no_quote"
+    echo "switch1_mac{switch1_mac_no_quote=\"${switch1_mac_no_quote}\"} 1"
 else 
     echo "host2_snmp_mac_status{host=\"${host2}\"} 0"
 fi

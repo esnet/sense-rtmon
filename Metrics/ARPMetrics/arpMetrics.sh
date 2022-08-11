ae2d0a8e0bc5dab632848e0e338e3bf24347b807
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-08-11 14:45:10 -0400

    outdated files

--- Metrics/ARPMetrics/arpMetrics.sh
+++ Metrics/ARPMetrics/arpMetrics.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-# pip3 install pyyaml
-# pip3 install prometheus_client
-# python3 hostMetrics.py & ( sleep 2 && python3 overwrite_json_exporter_arp.py )
 python3 overwrite_json_exporter_arp.py
-# trap "kill -- -$$" EXIT

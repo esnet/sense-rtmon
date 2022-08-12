3abb8d4b60f0ffe595ea8c30c8304b0b2c594306
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-08-12 14:19:36 -0400

    Update clean.sh

--- site/clean.sh
+++ site/clean.sh
@@ -3,11 +3,8 @@ echo "!!    Removing related Stack, Containers, Compose"
 systemctl stop node_exporter
 docker stack rm site
 docker compose down -v
-docker rm -f node-exporter snmp-exporter arp-exporter tcp-exporter arpexporter tcpexporter
-# remove compose containers
+
 docker rm -f compose-files-snmp-exporter-1 compose-files-tcp-exporter-1 compose-files-arp-exporter-1 compose-files-node-exporter-1
-# docker image rm -f quay.io/prometheus/node-exporter prom/snmp-exporter
-docker image rm -f site_tcp-exporter site_arp-exporter site_tcp-exporter arp_exporter:latest tcp_exporter:latest
 
 rm -rf ./crontabs/push_snmp_exporter_metrics*.sh ./crontabs/push_node_exporter_metrics*.sh  ./crontabs/update_arp_exporter*
 

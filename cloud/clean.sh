1f57d65152d90420b96f24d68ca54fd6768d343b
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-08-17 08:33:35 -0400

    yaml update

--- cloud/clean.sh
+++ cloud/clean.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
 echo "!!    Delete Cloud Stack"
-# docker rm -f startpush startprom startgrafana
 docker stack rm cloud
 sudo systemctl stop grafana-server
-
-# echo "!!    Wait for the containers to shut down"
-# sleep 1.5
+echo "!!    Wait for 2s for the containers to shut down"

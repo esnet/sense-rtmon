c0e19e6eb9df121177f50896eb80b9b863af34da
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-08-31 22:27:09 -0400

    Update start.sh

--- cloud/start.sh
+++ cloud/start.sh
@@ -35,8 +35,12 @@ fi
 yes | cp -rfa se_config/. script_exporter/examples
 docker stack deploy -c docker-stack.yml cloud
 
-echo "!!    Before Generating Dashboard for the first time please setup Grafana authorization API key"
-# echo "!!    Visit Google Doc for Grafana API and add Promethues as a Data Source Key instruction: https://docs.google.com/document/d/e/2PACX-1vRAwtpqlMKbii-hiqMoFD_N5PghMSw2eTMts9VhBww3AoSnXnQkjEcra4ReyLLsXrAuE_VEwLHRg33c/pub"
+echo "!!    IMPORTANT:"
+echo "!!    Before Generating Dashboard for the first time please setup Data Source and Grafana authorization API key"
+echo "!!    API key can be done automatically but Data Source needs to be configured MANNUALLY:"
+echo "!!    Visit Google Doc for Grafana API and add Promethues as a Data Source "
+echo "!!    Instruction: https://docs.google.com/document/d/e/2PACX-1vRAwtpqlMKbii-hiqMoFD_N5PghMSw2eTMts9VhBww3AoSnXnQkjEcra4ReyLLsXrAuE_VEwLHRg33c/pub"
+
 read -r -p "AUTO setup AUTH API keys? [y/N enter]: " API 
 if [ "$API" == "y" ] || [ "$API" == "Y" ]; then
     read -r -p "username (default is admin): " username 

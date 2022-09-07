596221140cc361809d26f8c1ed2c46d0ae7c1dea
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-09-07 12:59:17 -0400

    Update fill_API.py

--- cloud/fill_API.py
+++ cloud/fill_API.py
@@ -17,6 +17,9 @@ os.chdir(owd)
 data = {}
 file_name = "config.yml"
 
+print("!!   AUTO CURL MAY FAIL")
+print("!!    Visit Google Doc for Grafana API and add Promethues as a Data Source Key instruction: https://docs.google.com/document/d/e/2PACX-1vRAwtpqlMKbii-hiqMoFD_N5PghMSw2eTMts9VhBww3AoSnXnQkjEcra4ReyLLsXrAuE_VEwLHRg33c/pub")
+
 # argument given
 if len(sys.argv) > 1:
     file_name = str(sys.argv[1])

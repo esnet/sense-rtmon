4a4d813d6f1a2e8879a58998f0b808eff3fe4ac9
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-09-14 09:33:42 -0400

    Update certify.sh

--- cloud/certify.sh
+++ cloud/certify.sh
@@ -1,9 +1,48 @@
 #! /bin/bash
 
 docker run --rm -it -v /etc/letsencrypt:/etc/letsencrypt -p 80:80 certbot/certbot certonly \
---standalone -n --agree-tos \
+-n --agree-tos \
 --register-unsafely-without-email \
 --domains $1
+# --standalone 
+if [ -f "/opt/certbot/live/$1/fullchain.pem" ]; then
+    echo "!!    Copying certificates from /opt/certbot/live/$1/ to /opt/sense-rtmon/tls/"
+    mkdir -p /opt/sense-rtmon/tls/
+    # /etc/pki/tls
+    cd /opt/sense-rtmon/tls/
+
+    cp /opt/certbot/live/$1/fullchain.pem tls.crt
+    cp /opt/certbot/live/$1/privkey.pem tls.key
+    chmod 0777 tls.crt
+    chmod 0777 tls.key
+    # # Building keystore.
+    # openssl pkcs12 -export -in tls.crt -inkey tls.key \
+    #     -certfile tls.crt -out keystore.p12 -password pass:$tls_pass
+    
+    # # Converting keystore.
+    # rm -f server.keystore    
+    # keytool -importkeystore \
+    #     -srckeystore keystore.p12 -srcstoretype pkcs12 -srcstorepass $tls_pass \
+    #     -destkeystore server.keystore -deststoretype JKS -deststorepass $tls_pass
+    # rm -f keystore.p12
+
+    # # Configuring keystore.
+    # keytool -changealias -alias 1 -destalias server -keystore server.keystore -storepass $tls_pass
+    # chmod 0777 server.keystore
+
+        sudo tee ./nginx/server_conf<<EOF
+server_name $1;
+ssl_certificate     "/opt/sense-rtmon/tls/tls.crt";
+ssl_certificate_key "/opt/sense-rtmon/tls/tls.key";
+EOF
+
+first_line="proxy_pass http://$1:3000/;"
+sed -i.bak "1s/.*/$first_line/" ./nginx/proxy_conf
+
+else
+    echo "!!    Certificate not found! Error during TLS configuration."
+    exit 1
+fi
 
 # usage() {
 #     cat <<EOF

54c4f0fbfa971dc8c05b0a2df23dabfa794e4997
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-10-10 14:20:41 -0400

    change in json

--- cloud/dashboard/api.py
+++ cloud/dashboard/api.py
@@ -10,29 +10,34 @@ owd = os.getcwd()
 os.chdir("..")
 os.chdir("..")
 config_path = str(os.path.abspath(os.curdir)) +"/config_flow"
-infpth = config_path + "/config.yml"
 os.chdir(owd)
 data = {}
-with open(infpth, 'r') as stream:
+
+# given config file
+file_name = str(sys.argv[3])
+file_path = config_path + "/" + file_name
+print(f"\n Config file {file_path}\n")
+with open(file_path, 'r') as stream:
     try:
         data = yaml.safe_load(stream)
     except yaml.YAMLError as exc:
-        pass
-
-server = "http://" + str(data['grafanaHostIP']) + ":" + str(data['grafanaPort'])
-    
+        print(f"\n Config file {file_path} could not be found in the config directory\n")
+            
 # Get Default Home Dashboard
-url = server + "/api/dashboards/db"
+url = f"http://{str(data['grafanaHostIP'])}:{str(data['grafanaPort'])}/api/dashboards/db"
+
 # HTTP Post Header
 # Replace with your Grafana API key
 headers = {"Authorization": str(data['grafanaAPIToken']),
             "Content-Type": "application/json",
             "Accept": "application/json"}
+
 # Open and load out.json input
 f = open(sys.argv[1],)
 f2 = open(sys.argv[2],)
 x = json.load(f)
 x2 = json.load(f2)
+
 # HTTP Post Request
 r = requests.post(url=url, headers=headers, data=json.dumps(x), verify=False)
 print(r.json())

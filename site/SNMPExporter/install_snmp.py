97ad66fda087cc3f8c8fd0843e0e1f02bb3e45e6
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-10-13 12:59:58 -0400

    Update install_snmp.py

--- site/SNMPExporter/install_snmp.py
+++ site/SNMPExporter/install_snmp.py
@@ -3,7 +3,7 @@ import subprocess
 import os
 import yaml
 import site
-from .. import site_functions
+from ..SNMPExporter import site_functions
 
 # read yml file
 data,file_name = site_functions.read_yml_file("config_site",sys.argv,1,2)

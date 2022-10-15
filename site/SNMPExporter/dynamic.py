97c11e0e4fb71175e5beba353b457ab9fe56d183
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-10-15 15:43:38 -0400

    Update dynamic.py

--- site/SNMPExporter/dynamic.py
+++ site/SNMPExporter/dynamic.py
@@ -10,18 +10,17 @@ data,file_name = site_functions.read_yml_file("config_site",sys.argv,1,2)
 print("Collecting SNMP generator template...")
 print("Reading SNMP OIDs/Interfaces/Scrape Duration/Scrape Time from config file...")
 
-mib_dir = f"{str(os.getcwd())}/src/github.com/prometheus/snmp_exporter/generator"
+mib_dir = f"{str(os.getcwd())}/src/github.com/prometheus/snmp_exporter/generator/mibs"
 print(f"\n\nset MIBDIRS to MIBDIRS={mib_dir}")
-os.environ["MIBDIRS"]= f":{mib_dir}"
-# subprocess.run(f"export MIBDIRS=$MIBDIRS:{mib_dir}", shell=True, cwd=genLoc)
+# os.environ["MIBDIRS"]= f"{os.getenv["MIBDIRS"]}:{mib_dir}"
+subprocess.run(f"export MIBDIRS=$MIBDIRS:{mib_dir}", shell=True, cwd=genLoc)
 print("SNMP and MIBs install complete.")
 
 # file naming 
 for i in range(int(data['switchNum'])):
-    letter = chr(ord('A')+i)
+    letter = chr(ord('A')+i) # A B C D ... 
     site_functions.write_template(data,order_letter=letter)
-    site_functions.generate_snmp_file(f"snmp{str(i+1)}.yml")
-    
+    site_functions.generate_snmp_file(f"snmp{str(i+1)}.yml") # snmp1.yml snmp2.yml ...
 else:
     print("invilad switch number")
     exit

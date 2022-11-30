773765fdba1704e2d5e0ea090f384dc0fe9c03e5
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-11-30 12:30:06 -0500

    correct path

--- site/snmp/dynamic_start.sh
+++ site/snmp/dynamic_start.sh
@@ -4,6 +4,7 @@
 MYIP=1.1.1.1
 pushgateway_server=dev2.virnao.com:9091
 switch_target=10.10.10.10
+private_mib=placeholder
 ############################# PYTHON SCRIPT FILL OUT ####################
 
 ############################# CRONTAB ###################################
@@ -13,7 +14,7 @@ echo "Starting Crontab setup"
 
 echo "#Puppet Name: snmp exporter send data to pushgateway every 15 seconds" >> cron_autopush
 echo "MAILTO=""" >> cron_autopush
-echo "* * * * * for i in 0 1 2; do ${PWD}/crontabs/push_snmp_exporter_metrics.sh & sleep 15; done; ${PWD}/crontabs/push_snmp_exporter_metrics.sh" >> cron_autopush
+echo "* * * * * for i in 0 1 2; do /home/push_snmp_exporter_metrics.sh & sleep 15; done; /home/push_snmp_exporter_metrics.sh" >> cron_autopush
 crontab cron_autopush
 echo "!!    crontab set up successfully"
 
@@ -25,11 +26,11 @@ chmod 755 push_snmp_exporter_metrics.sh
 tee push_snmp_exporter_metrics.sh<<EOF
 #! /bin/bash
 if curl ${MYIP}:9116/metrics | grep ".*"; then
-    curl -o ${general_path}/site/crontabs/snmp_temp.txt ${MYIP}:9116/snmp?target=${switch_target}&module=if_mib
+    curl -o /home/snmp_temp.txt ${MYIP}:9116/snmp?target=${switch_target}&module=if_mib
 else
-    > ${general_path}/site/crontabs/snmp_temp.txt	
+    > /home/snmp_temp.txt	
 fi
-cat ${general_path}/site/crontabs/snmp_temp.txt | curl --data-binary @- ${pushgateway_server}/metrics/job/snmp-exporter/target_switch/${switch_target}/instance/${MYIP}
+cat /home/snmp_temp.txt | curl --data-binary @- ${pushgateway_server}/metrics/job/snmp-exporter/target_switch/${switch_target}/instance/${MYIP}
 EOF
 
 ############################# Start SNMP #############################
@@ -38,6 +39,13 @@ echo "!!    Correct MIBS environment variable"
 echo "!!    Move generator.yml file to SNMP Exporter generator folder"
 yes | cp -rfa /home/generator.yml /home/snmp_exporter/generator/
 echo "!!    Generating snmp.yml file"
+
+# I included a few common network elements, be sure yours is included. Avoid including all elements, it will take a long time with warnings to generate SNMP.yml file later
+export MIBDIRS=/home/librenms/mibs/
+export MIBDIRS=${MIBDIRS}:/home/librenms/mibs/cisco/
+export MIBDIRS=${MIBDIRS}:/home/librenms/mibs/dell/
+# export MIBDIRS=${MIBDIRS}:/home/librenms/mibs/${private_mib}
+
 cd /home/snmp_exporter/generator
 go build
 ./generator generate

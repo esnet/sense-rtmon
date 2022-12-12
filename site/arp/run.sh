a0b4cc80278b2b39e6e99372ab57f2a5ec19e895
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-12-12 15:44:25 -0500

    attempt new arp exporter

--- site/arp/run.sh
+++ site/arp/run.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 cd home
-# export PATH=$PATH:/usr/local/go/bin
 
 echo "!!    Run dynamic_start.sh"
 ./dynamic_start.sh
 
 echo "!!    Start ARP Exporter"
 
-python3 arp_exporter.py
+python3 cleaner_arp_exporter.py

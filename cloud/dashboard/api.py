bce61286ff551417a28af2a2fd83b64594317c04
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2023-03-07 00:30:26 -0500

    Dynamic flow and dashboard generation completed
    New generation is tested and working

--- cloud/dashboard/api.py
+++ cloud/dashboard/api.py
@@ -11,12 +11,12 @@ print("\n\nParsing config file...")
 data,file_name = cloud_functions.read_yml_file("config_flow",sys.argv,2,2)
             
 # Get Default Home Dashboard
-url = f"http://{str(data['grafanaHostIP'])}:{str(data['grafanaPort'])}/api/dashboards/db"
-# url = f"{str(data['grafana_host'])}/api/dashboards/db"
+# url = f"http://{str(data['grafanaHostIP'])}:{str(data['grafanaPort'])}/api/dashboards/db"
+url = f"{str(data['grafana_host'])}/api/dashboards/db"
 
 # HTTP Post Header
 # Replace with your Grafana API key
-headers = {"Authorization": str(data['grafanaAPIToken']), 
+headers = {"Authorization": str(data['grafana_api_token']), 
            "Content-Type": "application/json",
             "Accept": "application/json"}
 

4bfe40a79205575d8a6b571d1c03a95f4b86eafd
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2023-03-13 14:51:22 -0400

    Update dynamic_start.sh

--- site/node_mqtt/dynamic_start.sh
+++ site/node_mqtt/dynamic_start.sh
@@ -6,7 +6,10 @@ echo "Starting Crontab setup"
 echo "#Puppet Name: node exporter send data to pushgateway every 15 seconds" >> cron_autopush
 echo "MAILTO=""" >> cron_autopush
 echo "* * * * * for i in 0 1 2; do /home/push_node_exporter_metrics.sh & sleep 15; done; /home/push_node_exporter_metrics.sh" >> cron_autopush
-echo "* * * * * for i in 0 1 2; do python3 /home/update.py & sleep 15; done; /home/update.py" >> cron_autopush
+
+echo "#Puppet Name: check Cloud message every 15 seconds" >> cron_autopush
+echo "MAILTO=""" >> cron_autopush
+echo "* * * * * for i in 0 1 2; do python3 /home/update.py & sleep 15; done; python3 /home/update.py" >> cron_autopush
 crontab cron_autopush
 echo "!!    crontab set up successfully"
 

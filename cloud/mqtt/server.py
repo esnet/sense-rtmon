27d67ed8a500fb8b04399dee08350a9bfb75198b
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2023-03-13 14:34:10 -0400

    testing

--- cloud/mqtt/server.py
+++ cloud/mqtt/server.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqtt
 import json
+import time 
 
 # Define the MQTT broker and topic
 broker_address = "dev2.virnao.com"
@@ -12,12 +13,19 @@ client = mqtt.Client()
 # Connect to the MQTT broker
 client.connect(broker_address,port=port_num)
 
+example_data = {
+    "exporter": "node",
+    "pushgateway": "dev2.virnao.com:9091",
+    "name":"my_computer",
+    "status": 1
+}
+
 # Create a message in JSON format
-message_dict = {"exporter": "node", "status": 1}
+message_dict = example_data
 message_json = json.dumps(message_dict)
 print("sending message: " + message_json)
-# Publish the message to the topic
-client.publish(topic, message_json)
 
-# Disconnect from the MQTT broker
-# client.disconnect()
+while True:
+    # Publish the message to the topic
+    client.publish(topic, message_json)
+    time.sleep(5)
--- site/node_mqtt/dynamic_start.sh
+++ site/node_mqtt/dynamic_start.sh
@@ -6,6 +6,7 @@ echo "Starting Crontab setup"
 echo "#Puppet Name: node exporter send data to pushgateway every 15 seconds" >> cron_autopush
 echo "MAILTO=""" >> cron_autopush
 echo "* * * * * for i in 0 1 2; do /home/push_node_exporter_metrics.sh & sleep 15; done; /home/push_node_exporter_metrics.sh" >> cron_autopush
+echo "* * * * * for i in 0 1 2; do python3 /home/update.py & sleep 15; done; /home/update.py" >> cron_autopush
 crontab cron_autopush
 echo "!!    crontab set up successfully"
 
@@ -17,6 +18,9 @@ curl -s localhost:${NODE_PORT}/metrics | curl --data-binary @- ${PUSHGATEWAY_SER
 EOF
 chmod 755 push_node_exporter_metrics.sh
 crond
+
+# run mqtt client in python3 in the background
+nohup /home/client.py &
 
 # run the node exporter from github
 cd node_exporter

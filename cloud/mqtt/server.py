c1a58ea0b70ec4f436291d2e1fd79cb1d28b5c98
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2023-03-13 10:18:52 -0400

    Update server.py

--- cloud/mqtt/server.py
+++ cloud/mqtt/server.py
@@ -2,7 +2,7 @@ import paho.mqtt.client as mqtt
 import json
 
 # Define the MQTT broker and topic
-broker_address = "http://dev2.virnao.com"
+broker_address = "dev2.virnao.com"
 topic = "example/topic"
 
 # Create an MQTT client instance

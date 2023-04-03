f04af4bc2cbebf1196e08db419f695ec6506c027
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2023-04-03 00:06:22 -0400

    Update dynamic.py

--- cloud/dashboard/dynamic.py
+++ cloud/dashboard/dynamic.py
@@ -124,6 +124,7 @@ for i,node in enumerate(data["node"],i):
     l2table = replace_file_to_string("./templates/l2_debugging_panel/table.json",rep)
 
     formatted_name = node['name'].replace("-", "_").replace(".", "_").upper()
+    print(formatted_name)
     for i in range(1,4):
         rep[f"NODENAME_SCRIPT_EXPORTER_TASK{i}"] = f"{formatted_name}_SCRIPT_EXPORTER_TASK{i}"
     node_target = replace_file_to_string(f"./templates/l2_debugging_panel/{node['type']}_target.json",rep)

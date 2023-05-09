e03e2c44cd7b2b5b2ddfed011d65cb87d95a7337
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2023-05-09 12:10:10 -0400

    Update flow_to_api.py

--- cloud/orchestrator/flow_to_api.py
+++ cloud/orchestrator/flow_to_api.py
@@ -109,8 +109,12 @@ if __name__ == "__main__":
         
     # for node in node_data:
     #     print(node)
-    
-    params = {'hostname': 'https://sense-caltech-fe.sdn-lb.ultralight.org', 'sitename': 'T2_US_Caltech_Test'} # given by orchestrator
+    if 'hostname' not in data or 'sitename' not in data:
+        params = {'hostname': 'https://sense-caltech-fe.sdn-lb.ultralight.org', 'sitename':   'T2_US_Caltech_Test'} # given by orchestrator
+    else: 
+        hostname = data['hostname']
+        sitename = data['sitename']
+        params = {'hostname': hostname, 'sitename': sitename} # given by orchestrator
     api = SiteRMAPI(**params,node_data=node_data)
     api.test()
     print("API Request Completed")
